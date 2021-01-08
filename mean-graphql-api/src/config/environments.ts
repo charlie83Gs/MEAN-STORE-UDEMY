2d62828b1173974f4c7c3ce75b798e7d44ba71d2
Charlie <carlosgomezsoza@gmail.com>
2021-01-08 14:41:36 -0600

    .env fix

--- mean-graphql-api/src/config/environments.ts
+++ mean-graphql-api/src/config/environments.ts
@@ -1,14 +1,10 @@
+import console from "console";
 import dotenv from "dotenv";
+import fs from 'fs';
 
-var environment = dotenv.config({ path: "./src/.env" });
-
-try {
-    environment = dotenv.config({
-    path: "./src/.env",
-  });
-} catch (error) {
-    environment = dotenv.config({ path: "./src/.env" });
-}
+//include a dummy env as the .env file is not included in the repo
+var env_file  = fs.existsSync("./src/.env") ? "./src/.env" : "./src/dummy.env";
+const environment = dotenv.config({ path: env_file});
 
 if (process.env.MEAN_STORE_NODE_ENV !== "production") {
   if (environment.error) {
