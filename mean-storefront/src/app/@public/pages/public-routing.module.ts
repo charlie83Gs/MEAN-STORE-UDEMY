644d9a852bc3a7a737d51947c661cb7bb92ac9b9
Charlie <carlosgomezsoza@gmail.com>
2021-01-08 17:40:11 -0600

    basic public frontent

--- mean-storefront/src/app/@public/pages/public-routing.module.ts
+++ mean-storefront/src/app/@public/pages/public-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
     component: PublicComponent,
     children: [
       {
-        path: `home`, loadChildren: () =>
+        path: ``, loadChildren: () =>
           import('./home/home.module').then(m => m.HomeModule)
       },
       {
--- mean-storefront/src/app/app-routing.module.ts
+++ mean-storefront/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: ``, redirectTo: `home`, pathMatch: `full` },
+  { path: ``, redirectTo: ``, pathMatch: `full` },
   { path: `**`, redirectTo: `not_found`, pathMatch: `full` }
 ];
 
